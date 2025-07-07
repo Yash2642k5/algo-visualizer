6e0dca708c29a6290c9227f8206716f022df4c68
Yash2642k5 <143917289+Yash2642k5@users.noreply.github.com>
2025-07-07 23:10:49 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -6,7 +6,7 @@ const path = require('path');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 10000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
